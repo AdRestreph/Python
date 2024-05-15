5880915e04c4095d023c2ec16f4862761e5f0c61
Andres David Restrepo Hernandez <adrestreph@eafit.edu.co>
2024-05-15 16:01:34 -0500

    Upgrade in the exercise

---
+++ Practica/nump/test.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+a = np.array([1, 2, 3, 4])
+print("Array 1D")
+print(a)
+print()
+b = np.array([(1, 2, 3, 4),(5, 6, 7, 8)])
+print("Array 2D")
+print(b)
+
+#Numpy ofrece mayor velocidad y menor espacio en memoria utilizado en comparacion con las listas y matrices originarias de Python
+
--- Practica/triangles.py
+++ Practica/triangles.py
@@ -1,20 +1,41 @@
-def equilateral(a:float,b:float,c:float)->bool:
-    if (a==b and a==c):
-        return True
-    return False
+def triangleValidation(a:float, b:float, c:float)->bool:
+    """Verify if the sides are >0 in a triangle.
+    
+    :param side a: float
+    :param side b: float
+    :param side c: float
+    :return: bool - If the sides are <= 0 return False// Use the Triangle inequality validation
+    """
+    #verify if the sides are > 0
+    if(a > 0 and b > 0 and c > 0):
+        #verify the triangle inequality
+        if((a + b >=c) or (b + c >=a) or (c + a >=b)):
+            return True
+    else:
+        print("Sides error: Not is a valid triangle")
+        return False
 
-print("This triang is equilateral: " + str(equilateral(3,3,3)))
+def typeTriangle(a:float, b:float, c:float)->str:
+    """Verify the type of triangle.
+    
+    :param validation: Use the function triangleValidation. if True, clasifing the triangle
+    :param side a: float
+    :param side b: float
+    :param side c: float
+    :return: str - The type of triangle
+    """
+    validation= triangleValidation(a,b,c)
+    result = "Not is possible the classification"
+    if validation:
+        #Validation equilateral triangle
+        if (a==b and a==c):
+            result = "triangle equilateral"
+        #Validation isosceles triangle
+        elif((a == b and a != c) or (a == c and a != b) or (b == c and b != a)):
+            result = "triangle isosceles"
+        #Scalene triangle
+        else:
+            result = "triangle scalene"
+    return result
 
-def isosceles(a:float,b:float,c:float)->bool:
-    if((a == b and a != c) or (a == c and a != b) or (b == c and b != a)):
-        return True
-    return False
-
-print("This triang is isosceles: " + str(isosceles(2,3,2)))
-
-def scalene(a:float,b:float,c:float)->bool:
-    if(a != b and a != c and b != c):
-        return True
-    return False
-
-print("This triang is scalene: " + str(scalene(2,3,4)))
+print(typeTriangle(0,3,3))
