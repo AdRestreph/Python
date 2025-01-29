d37b26db5de25ff7b6d0f42b86f886e27f7c0088
unknown <adrestreph@eafit.edu.co>
2025-01-29 14:06:04 -0500

    Se realiza el abordaje completo de la matriz de cada imagen

--- ReconocerIA/Tareas/Tarea1/Tarea1.py
+++ ReconocerIA/Tareas/Tarea1/Tarea1.py
@@ -8,12 +8,23 @@ image = plt.imread("C:/Users/Usuario/Pictures/aguilarB.jpg")
 image_gray = np.dot(image[..., :3], [0.2989, 0.5870, 0.1140])  # RGB a escala de grises
 
 # Convierte la imagen en matriz usando NumPy
-matrix = np.array(image_gray)
-
+matrix_gray = np.array(image_gray)
+matrix_color = np.array(image)
 # Imprime las dimensiones de la imagen
-print("Dimensiones:", matrix.shape)
+print("Dimensiones en gris:", matrix_gray.shape)
+print("Dimensiones en color:", matrix_color.shape)
 
-# Muestra la imagen usando matplotlib 
+# Configuración para que NumPy imprima todos los valores de la matriz:
+
+#np.set_printoptions(threshold=np.inf, suppress=True, linewidth=matrix_gray.shape[1] * 10)
+#np.set_printoptions(threshold=np.inf, suppress=True, linewidth=matrix_color.shape[1] * 10)
+
+# Imprime la matriz con valores completos
+
+#print("Matriz en gris:\n", matrix_gray)
+print("Matriz en color:\n", matrix_color)
+
+# Muestra la imagen usando matplotlib
 plt.imshow(image_gray, cmap='gray')
 plt.title("El mas bravo del mundo")
 plt.axis("off")
