fd5c414080cee621aea772caba514a7369cc0bbb
unknown <adrestreph@eafit.edu.co>
2025-02-12 11:51:14 -0500

    Correction in the functions

--- Proyectos/ReconocerIA/modify_images.py
+++ Proyectos/ReconocerIA/modify_images.py
@@ -2,42 +2,39 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-def directorys(save_path: str, iterable:int, matrix:set) -> str:
+def directorys(save_path: str, list_grayscale_images:list) -> None:
     """
     This function save the images in the directory/folders.
     ----------
     save_path : [str] - directory path in which the images will be saved
-    iterable : [int] - iterable to name the images
-    matrix : [set] - image matrix in grayscale
+    list_grayscale_images : [list] - list images/matrix in grayscale
     ----------
-    return: [str] - message
+    return: [None]
     """
+    for i in range(len(list_grayscale_images)):
+        plt.imsave(save_path + f'gray_{str(i+1)}.png', list_grayscale_images[i], cmap='gray')
 
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
-
-    plt.imsave(os.path.join(save_path, f'gray_{iterable}'), matrix, cmap='gray')
-    message = print(f'Images converted to grayscale: {iterable}')
-
-    return message
-
-def modify_images( path: str,save_path: str) -> None:
+def modify_images( path: str) -> list:
     """
-    This function modifies the images in a directory and use the directorys function.
+    This function modifies the images in a directory.
     ----------
     path : [str] - directory path
     save_path : [str] - path to save grayscale images
     ----------
-    return: None
+    return: []
     """
     list_dir = os.listdir(path)
+    grayscale_images = []
 
     for i in list_dir:
         image = plt.imread(os.path.join(path, i))
         image_gray = np.dot(image[..., :3], [0.2989, 0.5870, 0.1140])
         matrix_gray = np.array(image_gray)
-        directorys(save_path, i, matrix_gray)
+        grayscale_images.append(matrix_gray)
+
+    return grayscale_images
 
 path = 'C:/Users/Usuario/Pictures/images/images_color/'
 save_path = 'C:/Users/Usuario/Pictures/images/images_gray/'
-modify_images(path, save_path)
+
+directorys(save_path,modify_images(path))
