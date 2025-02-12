90dd9373243ebe7c7685e72a72a7e4a5914d9a7f
unknown <adrestreph@eafit.edu.co>
2025-02-12 10:45:55 -0500

    Chage the structure in the functions

--- Proyectos/ReconocerIA/modify_images.py
+++ Proyectos/ReconocerIA/modify_images.py
@@ -2,18 +2,24 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-def list_directory(path: str) -> list:
+def directorys(save_path: str, iterable:int, matrix:set) -> list:
     """
-    This function converts directory/files into a list of images.
+    This function save the images in the directory/folders.
     ----------
     path : [str] - directory path
     ----------
     return: [list]
     """
-    files = os.listdir(path)
-    return files
 
-def modify_images(list_dir: list, path: str, save_path: str) -> str:
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    plt.imsave(os.path.join(save_path, f'gray_{iterable}'), matrix, cmap='gray')
+    message = print(f'Images converted to grayscale: {iterable}')
+
+    return message
+
+def modify_images( path: str,save_path: str) -> None:
     """
     This function modifies the images in a directory and saves them in grayscale.
     ----------
@@ -23,19 +29,14 @@ def modify_images(list_dir: list, path: str, save_path: str) -> str:
     ----------
     return: [str] confirmation message
     """
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
+    list_dir = os.listdir(path)
 
     for i in list_dir:
         image = plt.imread(os.path.join(path, i))
         image_gray = np.dot(image[..., :3], [0.2989, 0.5870, 0.1140])
         matrix_gray = np.array(image_gray)
-        plt.imsave(os.path.join(save_path, f'gray_{i}'), matrix_gray, cmap='gray')
-
-    message = print(f'Images converted to grayscale: {len(list_dir)}')
-    return message
+        directorys(save_path, i, matrix_gray)
 
 path = 'C:/Users/Usuario/Pictures/images/images_color/'
 save_path = 'C:/Users/Usuario/Pictures/images/images_gray/'
-list_dir = list_directory(path)
-modify_images(list_dir, path, save_path)
+modify_images(path, save_path)
